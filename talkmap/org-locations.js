dedcfa94c01453e9532f7b1563b64c95f84bad6a
Adriana Galer Rotaru <42140791+adrianagaler@users.noreply.github.com>
2023-06-25 02:12:08 -0400

    update map locations

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,11 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
-  ],
-  [
-    "London, UK",
-    51.5073219,
-    -0.1276473
-  ],
-  [
     "San Francisco, California",
     37.7792808,
     -122.4192362
   ],
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Bruxelles, Belgium",
+    50.83874559889915, 4.375116653031177
   ]
 ];
